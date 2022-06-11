e461646bad93f73f2780632d5e4e3d723adb012d
Patrik Radics <54003031+Codethier@users.noreply.github.com>
2022-06-11 22:27:36 +0200

    init

---
+++ projects/example/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {NgxCodethierComponent, NgxCodethierModule} from "ngx-codethier";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxCodethierModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
